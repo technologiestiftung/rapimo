523beddff3a15d59d4733ff4948a8e471096d3a7
guadiromero <guadalupe.romero@ts.berlin>
2024-06-05 13:21:21 +0200

    feat: provide multiblock input with all the columns and validate data

--- plumber1.R
+++ plumber1.R
@@ -43,26 +43,80 @@ peekInput <- function(n_records) {
 
 
 #* @post /calculate_multiblock
-#* @param new_values_json:json New values
-calculateMultiblock <- function(new_values_json) {
-# Load Berlin data and config
-data <- kwb.rabimo::rabimo_inputs_2020$data
-config <- kwb.rabimo::rabimo_inputs_2020$config
+#* @param input:json Input for a selection of blocks in JSON format
+calculateMultiblock <- function(input) {
+  # Convert JSON to dataframe
+  input <- fromJSON(input)
 
-# Convert JSON to dataframe
-new_values <- fromJSON(new_values_json)
+  # Define the required column names
+  required_columns <- c("code", "prec_yr", "prec_s", "epot_yr", "epot_s",
+                        "district", "total_area", "area_main", "area_rd",
+                        "main_fraction", "roof", "green_roof", "swg_roof",
+                        "pvd", "swg_pvd", "srf1_pvd", "srf2_pvd", "srf3_pvd",
+                        "srf4_pvd", "srf5_pvd", "road_fraction", "pvd_rd",
+                        "swg_pvd_rd", "srf1_pvd_rd", "srf2_pvd_rd", "srf3_pvd_rd",
+                        "srf4_pvd_rd", "sealed", "to_swale", "gw_dist", "ufc30",
+                        "ufc150", "land_type", "veg_class", "irrigation", "block_type")
 
-# Create a copy of the original data frame for manipulation
-updated_data <- data
+  # Check if all required columns are present
+  missing_columns <- setdiff(required_columns, names(input))
 
-# Override data with new values
-updated_data <- left_join(updated_data, new_values, by = "code", suffix = c("", ".new_values")) %>%
-  mutate(green_roof = coalesce(green_roof.new_values, green_roof)) %>%
-  select(-green_roof.new_values)  # Remove the temporary column
+  if (length(missing_columns) > 0) {
+    # Return a 400 Bad Request error response
+    response <- list(
+      error = "400 - Bad Request",
+      message = paste("Missing column(s):", paste(missing_columns, collapse = ", "))
+    )
+    return(response)
+  }
 
-# Run abimo calculations
+  # Convert specific columns to the appropriate data types
+  input <- input %>%
+    mutate(
+      code = as.character(code),
+      prec_yr = as.integer(prec_yr),
+      prec_s = as.integer(prec_s),
+      epot_yr = as.integer(epot_yr),
+      epot_s = as.integer(epot_s),
+      district = as.character(district),
+      total_area = as.numeric(total_area),
+      area_main = as.numeric(area_main),
+      area_rd = as.numeric(area_rd),
+      main_fraction = as.numeric(main_fraction),
+      roof = as.numeric(roof),
+      green_roof = as.numeric(green_roof),
+      swg_roof = as.numeric(swg_roof),
+      pvd = as.numeric(pvd),
+      swg_pvd = as.numeric(swg_pvd),
+      srf1_pvd = as.numeric(srf1_pvd),
+      srf2_pvd = as.numeric(srf2_pvd),
+      srf3_pvd = as.numeric(srf3_pvd),
+      srf4_pvd = as.numeric(srf4_pvd),
+      srf5_pvd = as.numeric(srf5_pvd),
+      road_fraction = as.numeric(road_fraction),
+      pvd_rd = as.numeric(pvd_rd),
+      swg_pvd_rd = as.numeric(swg_pvd_rd),
+      srf1_pvd_rd = as.numeric(srf1_pvd_rd),
+      srf2_pvd_rd = as.numeric(srf2_pvd_rd),
+      srf3_pvd_rd = as.numeric(srf3_pvd_rd),
+      srf4_pvd_rd = as.numeric(srf4_pvd_rd),
+      sealed = as.numeric(sealed),
+      to_swale = as.numeric(to_swale),
+      gw_dist = as.numeric(gw_dist),
+      ufc30 = as.numeric(ufc30),
+      ufc150 = as.numeric(ufc150),
+      land_type = as.character(land_type),
+      veg_class = as.integer(veg_class),
+      irrigation = as.integer(irrigation),
+      block_type = as.character(block_type)
+    )
+
+  # Load default configuration
+  config <- kwb.rabimo::rabimo_inputs_2020$config
+
+  # Run abimo calculations
   rabimo_result <- kwb.rabimo::run_rabimo(
-    data = updated_data,
+    data = input,
     config = config
 )
 
